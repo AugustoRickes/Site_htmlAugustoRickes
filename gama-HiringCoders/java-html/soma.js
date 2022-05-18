65722f8a7a851aed38983db3f5231d3aa0061c55
Augusto Rickes da Silveira <ag.augusto.rickes@gmail.com>
2022-05-18 18:19:50 -0300

    add func soma

---
+++ gama-HiringCoders/java-html/soma.js
@@ -0,0 +1,12 @@
+//criando as variaveis e inserindo valores
+
+var valor1 = 26549840;
+var valor2 = 745684652140;
+
+function soma(x,y) {
+
+    return x + y;
+
+}
+
+console.log(soma(valor1,valor2));
